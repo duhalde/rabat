15ee5260f3df470004adb3ddd953afa7e882c29d
Oliver <oliver@dtu>
2012-09-11 12:16:55 +0200

    Play back signals by use of PsychPortAudio

---
+++ Acquisition/SoundPlayback/SoundPlayback.m
@@ -0,0 +1,15 @@
+function y = SoundPlayback()
+%
+%   Description: Generate signals and playback
+%
+%   Usage: y = function_name(x,...)
+%
+%   Input parameters:
+%       - x: 
+%   Output parameters:
+%       - y: 
+%
+%   Author: Oliver Lylloff, Mathias Immanuel Nielsen & David Duhalde 
+%   Date: 11-9-2012, Last update: 11-9-2012
+%   Acoustic Technology, DTU 2012
+
