e6c214a357f4220f98ffd0be680b1bf0516024fd
Oliver <oliver@dtu>
2012-09-13 12:26:44 +0200

    Added signals 'linsin','sin','mls','irs'
    Still need some bugfixing for 'mls' and 'irs'

--- Acquisition/SoundPlayback/SoundPlayback.m
+++ Acquisition/SoundPlayback/SoundPlayback.m
@@ -1,36 +1,92 @@
-function y = SoundPlayback(f1,f2,fs,length_sec,sig_type)
+function y = SoundPlayback(sig_type,varargin)
 %
-%   Description: Generate log sine sweep and play
+%   Description: Generate signal from ...
 %
-%   Usage: y = SoundPlayback(f1,f2,fs,length_sec,sig_type)
-%
+%   Usage: y = SoundPlayback(sig_type,varargin)
+%   
 %   Input parameters:
-%       - f1: Lower frequency
-%       - f2: Upper frequency
-%       - fs: Sampling frequency
-%       - length_sec: Length of signal
 %       - sig_type: String specifing the type of signal to be generated
-%         'sin'     : Sine signal
-%         'linsin'  : Linear sine sweep
 %         'logsin'  : Exponential sine sweep
+%         'linsin'  : Linear sine sweep
+%         'sin'     : Sine signal
 %         'mls'     : MLS
 %         'irs'     : IRS
+%       - varargin: Variable input parameter list
 %
 %   Output parameters:
 %       - y: sampled signal
+% 
+%   Details on signal types and input arguments:
+% 
+%   Signal types
+%   ------------
+%   
+%   'logsin'    Creates a logarithmic sweep in the time domain. 
+%               Usage: y = SoundPlayback('logsin',fs,f1,f2,length_sig)
+%               Input parameters:  
+%               - fs: Sampling frequency
+%               - f1: Lower frequency
+%               - f2: Upper frequency
+%               - length_sec: Length of signal
 %
+%   'linsin'    Creates a linear sweep in the time domain.
+%               Usage: y = SoundPlayback('linsin',fs,f1,f2,length_sig)
+%               Input parameters:  
+%               - fs: Sampling frequency
+%               - f1: Lower frequency
+%               - f2: Upper frequency
+%               - length_sec: Length of signal
+%
+%   
+%   'mls'       ... etc
+% 
+% 
 %   Author: Oliver Lylloff, Mathias Immanuel Nielsen & David Duhalde 
-%   Date: 11-9-2012, Last update: 12-9-2012
+%   Date: 11-9-2012, Last update: 13-9-2012
 %   Acoustic Technology, DTU 2012
 % 
 %   TODO:   
-%       - Implement all signals
-%       - 
+%       - Make help text and make signals more similar, i.e make fs the 
+%           first input argument etc.
+%       - 'mls','irs' is missing helper function 'myprimpol'
 
-if (strcmp(sig_type,'logsin'))
-y = log_sine_sweep(f1,f2,fs,length_sec);
+switch lower(sig_type)
+    case 'logsin'
+        fs = varargin{1};
+        f1 = varargin{2};
+        f2 = varargin{3};
+        length_sec = varargin{4};
+  
+        y = log_sine_sweep(f1,f2,fs,length_sec);
+    
+    case 'linsin'
+        fs = varargin{1};
+        f1 = varargin{2};
+        f2 = varargin{3};
+        length_sec = varargin{4};
+        
+        y = lin_sine_sweep(f1,f2,fs,length_sec);
+        
+    case 'mls'
+        n = varargin{1};
+        
+        y = mls2(n);
+    
+    case 'sin'
+        fs = varargin{1};
+        f0 = varargin{2};
+        length_sec = varargin{3};
+        
+        y = gen_sin(f0,fs,length_sec);
+        
+    case 'irs'
+        n = varargin{1};
+        
+        y = irs2(n);
+    otherwise
+        error('Unknown method, choose one of: "logsin","linsin","mls","sin","irs"')
+end
 
-freq = fs;
 nrchannels = 1;    % One channel only -> Mono sound.
 wavedata = y;      % Need sound vector as row vector, one row per channel.
 
@@ -41,7 +97,7 @@ InitializePsychSound;
 % and a required latencyclass of zero 0 == no low-latency mode, as well as
 % a frequency of freq and nrchannels sound channels.
 % This returns a handle to the audio device:
-pahandle = PsychPortAudio('Open', [], [], 0, freq, nrchannels);
+pahandle = PsychPortAudio('Open', [], [], 0, fs, nrchannels);
 
 % Fill the audio playback buffer with the audio data 'wavedata':
 PsychPortAudio('FillBuffer', pahandle, wavedata);
@@ -62,7 +118,3 @@ PsychPortAudio('Stop', pahandle);
 
 % Close the audio device:
 PsychPortAudio('Close', pahandle);
-
-else
-  error('Illegal method')
-end
