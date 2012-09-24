be43285f2ce77e96a6e06547628283393b7595ce
David Duhalde Rahbæk <david@duhalde.dk>
2012-09-24 21:09:20 +0200

    something has been moved….

--- DeconvolutionAndOtherFunctions/sweepdeconv.m
+++ DeconvolutionAndOtherFunctions/sweepdeconv.m
@@ -15,9 +15,12 @@ function [h,t] = sweepdeconv(x,y,f1,f2,fs,varargin)
 %   Author: Toni Torras, Date: 3-1-2009, Last update: 26-6-2009    
 x = x(:);
 y = y(:);
+
 Xinv = fft([zeros(length(y)-1,1) ; flip(x)]);
 F = Xinv.*exp(-j*2*pi*(0:length(Xinv)-1).'/length(Xinv))./(Xinv.*conj(Xinv));
+
 Y = fft([y ; zeros(length(x)-1,1)]);
+
 if nargin == 6 && strcmpi(varargin{1},'filter')
    [bband,aband] = butter(2,[2*f1/fs floor(1e4*(2*f2/fs-eps))/1e4]);
    Hband = freqz(bband,aband,length(x)+length(y)-1,'whole');
@@ -25,7 +28,9 @@ if nargin == 6 && strcmpi(varargin{1},'filter')
 else
    h = ifft(F.*Y,'symmetric');
 end
+
 h = h.';
+
 if mod(length(h),2) == 0 % Even number of samples
    h = [h(length(h)/2+1:end) h(1:length(h)/2)];
    t = linspace(-length(h)/2/fs,(length(h)/2 - 1)/fs,length(h)); 
