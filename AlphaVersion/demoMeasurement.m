fa40e67b4c846dc745b6ad3764f83bd21528d5c8
Oliver <oliver@dtu>
2012-10-01 15:02:27 +0200

    Small changes to demo scripts

--- AlphaVersion/demoMeasurement.m
+++ AlphaVersion/demoMeasurement.m
@@ -3,7 +3,7 @@ clear all
 
 % Generate logarithmic sine sweep
 fs = 44100;         % Sampling frequency
-f1 = 500;          % Lower frequency
+f1 = 500;           % Lower frequency
 f2 = 5000;          % Upper frequency
 length_sig = 5;     % Duration of sweep in seconds
 zero_pad = 0;       % zero padding (default value)
@@ -12,8 +12,8 @@ phase = 0;          % Phase (default value)
 
 [sweep,t] = rbtGenerateSignal('logsin',fs,f1,f2,length_sig,zero_pad,amp,phase);
 
-%% Start measurement
-RT = 1;     % Estimated reverberation time of room
+% Start measurement
+RT = 1;             % Estimated reverberation time of room
 y = rbtMeasurement(sweep,fs,RT);
 
 % Compute impulse response
