cda93f637b11416d628a87aa8083ec884d31882e
Oliver <oliver@dtu>
2012-11-15 14:41:41 +0100

    Testing functions

--- Beta/testCrop.m
+++ Beta/testCrop.m
@@ -1,3 +1,4 @@
+clear all
 % Load impulse response from wav:
 [h,fs] = wavread('meas5NoDirac.wav');
 %h = h(:,1);
@@ -12,9 +13,12 @@ H = rbtIR2octBands(h,fs,cfmin,cfmax);
 
 [hCrop,idxStart,idxEnd] = rbtCropIR(H,fs);
 
+[knee, rmsNoise] = rbtLundeby(hCrop,fs);
+%%
 t = 0:1/fs:length(h)/fs-1/fs;
-for i = 1:size(H,2)
+for i = 7:7;%1:size(H,2)
 R = rbtBackInt(hCrop(:,i));
 [RT(i), r2p, dynRange] = rbtRevTime(R,t);
 plot(t,R), hold all
 end
+
