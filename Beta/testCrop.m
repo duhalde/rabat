76b48a67101959b0ae92791a5329ccb1d78a9064
Oliver <oliver@dtu>
2012-11-15 12:24:57 +0100

    Test script to run Crop, BackInt and RevTime

---
+++ Beta/testCrop.m
@@ -0,0 +1,20 @@
+% Load impulse response from wav:
+[h,fs] = wavread('sounds/church.wav');
+h = h(:,1);
+t = 0:1/fs:length(h)/fs-1/fs;
+
+% Frequency range of interest 
+cfmin = 63;             % lowest center frequency of interest
+cfmax = 8000;           % highest center frequency of interest
+
+% Filter IR into octave bands
+H = rbtIR2octBands(h,fs,cfmin,cfmax);
+
+[hCrop,idxStart,idxEnd] = rbtCropIR(H,fs);
+
+t = 0:1/fs:length(h)/fs-1/fs;
+for i = 1:size(H,2)
+R = rbtBackInt(hCrop(:,i));
+[RT(i), r2p, dynRange] = rbtRevTime(R,t);
+plot(t,R), hold all
+end
