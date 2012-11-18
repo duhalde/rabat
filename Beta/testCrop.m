3cd27ba218d06aae1f0634fc6076e9338a99cb89
Oliver <oliver@dtu>
2012-11-18 19:01:11 +0100

    Test accuracy of rabat

--- Beta/testCrop.m
+++ Beta/testCrop.m
@@ -1,8 +1,18 @@
 clear all
 close all
 % Load impulse response from wav:
-[h,fs] = wavread('sounds/Grundtvigs.wav');
-h = h(:,1);
+[hD,fsD] = wavread('Dirac/meas5NoDirac.wav');
+[measRabat3,fsR] = wavread('RABAT/logsin_12-Nov-2012-150952.wav');
+
+sig_type = 'logsin';
+fs = 48000;
+f1 = 22;
+f2 = 22*(fs/48);
+
+lengthSec = 5.46;
+[sweep,t] = rbtGenerateSignalWin(sig_type,fs,f1,f2,lengthSec);
+
+hR = sweepdeconv(sweep,measRabat3,f1,f2,fs);
 
 % Frequency range of interest 
 cfmin = 63;             % lowest center frequency of interest
@@ -12,20 +22,31 @@ cfmax = 8000;           % highest center frequency of interest
 freqs = rbtGetFreqs(cfmin,cfmax,1);
 
 % Crop IR
-[hCrop,idxStart,idxEnd,t] = rbtCropIR(h,fs);
+[hCropD,idxStartD,idxEndD,tD] = rbtCropIR(hD,fsD);
+[hCropR,idxStartR,idxEndR,tR] = rbtCropIR(hR,fsR);
 
 % Filter impulse response
-H = rbtIR2octBands(hCrop,fs,cfmin,cfmax);
+HD = rbtIR2octBands(hCropD,fsD,cfmin,cfmax);
+HR = rbtIR2octBands(hCropR,fsR,cfmin,cfmax);
 
 % Determine knee points and Noise floor level
-[knee, rmsNoise] = rbtLundeby(H,fs);
+[kneeD, rmsNoiseD] = rbtLundeby(HD,fsD);
+[kneeR, rmsNoiseR] = rbtLundeby(HR,fsR);
 
 % Get decay curves
-R = rbtBackInt(H);
+RD = rbtBackInt(HD);
+RR = rbtBackInt(HR);
 
 % Get reverberation time
-[RT, r2p, dynRange] = rbtRevTime(R,t);
+[RTD, r2pD, dynRangeD] = rbtRevTime(RD,tD,'all');
+[RTR, r2pR, dynRangeR] = rbtRevTime(RR,tR,'all');
 
-semilogx(freqs,RT,'-.')
+% Results from Dirac 
+T30 = [5.196 7.447 7.376 5.968 4.976 4.308 3.344 2.349 1.414 0.849];
+T20 = [6.141	7.531	7.309	5.892	4.989	4.312	3.333	2.284	1.379	0.937]; 
+%%
+semilogx(freqs,T20(2:end-1),'--o'), hold on
+plot(freqs,RTD,'k--s'), plot(freqs,RTR(1,:),'r--.')
 set(gca,'XTick',freqs)
-axis([60 8000 0 max(RT)])
+axis([60 8000 0 max(T30)+1])
+legend('Dirac-derived T30','RABAT derived from Dirac rec.','RABAT derived from RABAT rec.')
