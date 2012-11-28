1256cd31c170d9fc616497e73b589736dcb22468
Mathias Immanuel Nielsen <m.immanuel@gmail.com>
2012-11-28 19:44:40 +0100

    rbaCrossCorr

--- Beta/rbaCrossCorr.m
+++ Beta/rbaCrossCorr.m
@@ -1,6 +1,6 @@
-function [c, lags] = rbaCrossCorr(h1,h2)
+function [c, lags] = rbaCrossCorr(f, g)
 %$rbaCrossCorr  Robust implementation of Cross Correlation
-%   [c, lags] = rbaCrossCorr(h1,h2)
+%   [c, lags] = rbaCrossCorr(f, g)
 %
 %   Description:
 %       Since Matlabs built-in cross correlation sends calculations to
@@ -12,11 +12,10 @@ function [c, lags] = rbaCrossCorr(h1,h2)
 %       which consists of the response of several sweeps.
 %       The cross correlation searches for patterns, similar to h, in
 %       signal f. It is closely related to the convolution function
-%       (can you see the difference?)
 %
 %   Inputs:
-%       f:   the long signal (e.g. recorded measurement)
-%       h:   the short signal (e.g. prototype sweep)
+%       f:   Vector contaning the long signal (e.g. recorded measurement)
+%       g:   Vector contaning the short signal (e.g. prototype sweep)
 %
 %   Outputs:
 %       c:   the cross correlation of the to signals.
@@ -26,27 +25,27 @@ function [c, lags] = rbaCrossCorr(h1,h2)
 %   Example:
 %     	[c, lags] = rbaCrossCorr(multipleMeasuredSweeps,originalSweep)
 %
-% Author: David Duhalde Rahbæk & Mathias Immanuel Nielsen & Oliver Lylloff
-% Created: sep 5 2012 Updated: nov 28 2012
-% Copyright RaBaT Toolbox, DTU 2012
+%   Author: Oliver Lylloff, Mathias Immanuel Nielsen & David Duhalde 
+%   Date: 05-9-2012, Last update: 28-11-2012
+%   Acoustic Technology, DTU 2012
 
 
 %% Calculation of cross correlation
-h1 = h1(:);
-h2 = h2(:);
-Lh1 = length(h1);
-Lh2 = length(h2);
+f = f(:); % Som nedenstående
+g = g(:); % Det er et farligt trick!
+Lf = length(f);
+Lg = length(g);
 
-Ly = Lh1+Lh2-1;   % find length of cross correlation
+Ly = Lf+Lg-1;   % find length of cross correlation
 
 % to get the highest speed from fft() and ifft(),
 % the signal lengths should be a power of 2
 LyPow2 = pow2(nextpow2(Ly));  % find smallest power of 2 > Ly
 
 % perform fourier transform with zero-padding to length LyPow2
-H1 = fft(h1, LyPow2);     % fast Fourier transform
-H2 = fft(h2, LyPow2);	    % fast Fourier transform
-Y = H1.*conj(H2);         % muliply in frequency domain
+F = fft(f, LyPow2);     % fast Fourier transform
+G = fft(g, LyPow2);	    % fast Fourier transform
+Y = F.*conj(G);         % muliply in frequency domain
 
 % now go back to time-domain and force real values.
 c = real(ifft(Y, LyPow2));       % Inverse fast Fourier transform
@@ -55,6 +54,7 @@ c = real(ifft(Y, LyPow2));       % Inverse fast Fourier transform
 maxlag = Lh1-1;
 lags = -maxlag:maxlag;
 
+% crop output c to match lags
 c = [c(end-maxlag+1:end,:); c(1:maxlag+1,:)];
 
 
