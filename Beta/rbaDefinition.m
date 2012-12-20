8350e91d1665dd1857380048e2d5b2c021f741d7
Oliver <oliver@dtu>
2012-12-21 00:18:52 +0100

    Added onset to Definition

--- Beta/rbaDefinition.m
+++ Beta/rbaDefinition.m
@@ -1,4 +1,4 @@
-function D = rbaDefinition(h,fs,time)
+function D = rbaDefinition(ir,fs,time)
 %
 %   Description:    Calculate definition parameter (D50 by default) 
 %	in accordance with ISO-3382
@@ -31,6 +31,16 @@ else
     ir = ir(:,1);
 end
 
+% Determine proper onset of impulse response
+[~,idxstart] = max(ir);
+if idxstart > floor(5e-3*fs)
+idxstart = idxstart-floor(5e-3*fs);
+else
+    idxstart = 1;
+end
+ir = ir(idxstart:end);
+ir2 = ir.^2;
+
 % find index of integration time in ir
 int_idx = ceil(time*1e-3*fs);
 % error handling
@@ -39,6 +49,6 @@ if int_idx > length(ir)
 end
 
 % calculate Clarity parameter, see ISO-3382-1 (A.11)
-D = sum(ir(1:int_idx).^2)/sum(ir.^2);
+D = sum(ir2(1:int_idx))/sum(ir2);
 
 end
