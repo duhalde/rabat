7f78e643347aca68cc8be973eab09501a2e41dba
Oliver <oliver@dtu>
2012-12-20 22:35:50 +0100

    Updated EDT

--- Beta/rbaEDT.m
+++ Beta/rbaEDT.m
@@ -1,4 +1,4 @@
-function [EDT,r2p] = rbaEDT(R,t)
+function [EDT,r2p,stdDev] = rbaEDT(R,t)
 %
 %   Description: Calculate Early Decay Time (EDT) as the slope of the
 %   fitted decay curve from 0 dB to -10dB
@@ -16,6 +16,7 @@ function [EDT,r2p] = rbaEDT(R,t)
 %              to a perfect correlation between data and fit.
 %              The decay curve is non-linear for r2p > 10.
 %              According to ISO 3382-1:2009 (sec. B.3)
+%       - stdDev : Standard deviation of the linear regression fit.
 %
 %   Ref: ISO 3382-1:2009(E) section (A.2.2)
 %
@@ -41,31 +42,31 @@ if k<l
 end
 
 % Allocate
-L = zeros(m,n);
-rSqr = zeros(n,1);
-EDT = zeros(n,1);
-r2p = zeros(n,1);
-
+EDT = zeros(1,n);
+r2p = zeros(1,n);
+idx = zeros(1,n);
+dynRange = zeros(1,n);
+stdDev = zeros(1,n);
 for ii = 1:n
-    idx = find(R(:,ii)<-10,1,'first');    % Find first index where R is is below -10 dB
-    
-    coeff = polyfit(t(1:idx),R(1:idx,ii),1);    % Linear regression
-    
-    L(1:idx,ii) = coeff(1)*t(1:idx);    % Fitted curve
-    
-    % Compute r^2
-    r2p(ii) = nonLinCheck(R(1:idx,ii),L(1:idx,ii));
-    
-    % Check non-linearity according to ISO 3382-1:2009 (sec. B.3)
-    if 1000*(1-rSqr(ii)) > 10
-        warning('The decay curve looks non-linear. The calculated EDT may be wrong.')
+    idxFloor = find(R(:,ii)>min(R(:,ii)),1,'last');
+    idx(ii) = floor(0.95*length(R(1:idxFloor,ii)));  % Cut away the last 5%.
+    dynRange(ii) = R(1,ii)-R(idx(ii),ii);   % Calculate the dynamic range
+    if dynRange(ii)<15
+       warning('The dynamic range is less than 15 dB. The EDT might be untrustworthy.')
     end
-    
-    EDT(ii) = t(find(L(1:idx,ii)<-10,1,'first'));
+    idx10 = find(R(1:idx(ii),ii)<-10,1,'first');
+    coeff = polyfit(t(1:idx10),R(1:idx10,ii),1);
+    L = coeff(1)*t(1:idx10)+coeff(2);
+    r2 = nonLinCheck(R(1:idx10,ii),L(1:idx10));
     r2p(ii) = 1000*(1-r2);
+    if r2p(ii) > 10
+        warning('The decay looks non-linear.')
+    end
+    stdDev(ii) = std(R(1:idx10,ii)-L(1:idx10));
+    EDT(ii) = 6*(-10/coeff(1));
+end
 end
 
-end
 
 function rSqr = nonLinCheck(R1,L1)
 rSqr = sum((L1(:)-mean(R1)).^2)/sum((R1(:)-mean(R1)).^2);
