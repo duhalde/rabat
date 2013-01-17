ca9f6b7b52e9841be3897adaa71a673b0e3df6a6
Oliver <oliver@dtu>
2013-01-17 13:29:25 +0100

    Changes to demo-script

--- Beta/demos/demoAverageMeasurement.m
+++ Beta/demos/demoAverageMeasurement.m
@@ -3,30 +3,63 @@ clear all
 clc
 
 %% Generate logarithmic sine sweep
-fs = 48000;         % Sampling frequency
-f1 = 1000;           % Lower frequency
-f2 = 8000;          % Upper frequency
-length_sig = 3;     % Duration of sweep in seconds
 
-[sweep,t] = rbaGenerateSignal('logsin',fs,f1,f2,length_sig);
+% Define measurement signal parameters
 
-figure('Name','Spectrogram','Position',[0 400 400 400])
+%  We are interested in reverberation times in octave bands
+%   [125 250 500 1000 2000 4000]
+f1 = 125*2^(-1/2);
+f2 = 4000*2^(1/2);
+
+% For applying a time-window the sweep range must be wider than [f1:f2].
+% Note that Nyquist frequency must not be exceeded
+flow = f1/2;
+fup = f2*2;
+
+% Samplingfrequency should be greater than fup*2. 44.1kHz is very common
+% for built-in soundcards
+fs = 48e3;
+
+% Define signal type and length of measurement signal in seconds
+sig_type = 'logsin';
+length_sig = 3;
+
+% Generate measurement signal
+[y,t] = rbaGenerateSignal(sig_type,fs,flow,fup,length_sig);
+
+% Generate window
+L = length(y);
+win = sweepwin(L,flow,fup,f1,f2,sig_type);
+
+% Apply time-window function
+sweep = y.*win;
+
+figure(1)
+%figure('Name','Spectrogram','Position',[0 400 400 400])
 specgram(sweep)
-%% Start measurement
+%% Start measurement Steady-state
 N = 4;              % number of sweeps to average over
 y = rbaAverageMeasurement(sweep,fs,N);
 
+%% Start measurement transient
+N = 12; % Number of averages
+estimatedRT = 1; % Estimated reverberation time
+
+measuredResult = rbaMeasurement(sweep, fs, N, estimatedRT);
 %% Compute impulse response
-h = sweepdeconv(sweep,y,f1,f2,fs);
+h = sweepdeconv(measuredResult,y,f1,f2,fs);
 
 %%
-figure('Name','Spectrogram','Position',[0 400 400 400])
+figure(2)
+%figure('Name','Spectrogram','Position',[0 400 400 400])
 specgram(y,min(256,length(y)),fs)
 
-figure('Name','Impulse response','Position',[420 400 400 400])
+figure(3)
+%figure('Name','Impulse response','Position',[420 400 400 400])
 plot(h)
 
 %%
 [hCrop,t] = rbaCropIR(h,fs);
-figure('Name','Cropped Impulse response','Position',[420 400 400 400])
+figure(4)
+%figure('Name','Cropped Impulse response','Position',[420 400 400 400])
 plot(t,hCrop)
